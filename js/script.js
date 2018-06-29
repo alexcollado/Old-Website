41e66a3d0677c940ce889ece7cb8e0ed4482f6e7
alexcollado <alexc1117@gmail.com>
2018-06-29 03:16:39 -0400

    Added animejs library mostly for css animations. Made an underline animation for the homepage

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ $('.parallax-window').parallax({
     naturalHeight: 1080,
     speed: 0.4
 });
+
+var width = document.getElementById('title-name').offsetWidth;
+
+var cssProperties = anime({
+  targets: '#cssProperties .line',
+  width: width,
+  height: '5px',
+  duration: 4000,
+  delay: 300,
+});
+
+window.addEventListener('resize', function () { 
+    "use strict";
+    window.location.reload(); 
+});
